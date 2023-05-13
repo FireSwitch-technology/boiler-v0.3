a4a78b134ddcbefb16d30307efb45845b49fff4f
billycodes <billyhadiattaofeeq@gmail.com>
2023-05-13 07:27:26 +0100

    updated getAllUSERS ENDPOINT IN ADMIN AND INITILIALIZERS

--- v0.1/api/ztemplate.php
+++ v0.1/api/ztemplate.php
@@ -1,47 +1,22 @@
 <?php
-require_once( '../assets/initializer.php' );
-$data = ( array ) json_decode( file_get_contents( 'php://input' ), true );
+require_once('../assets/initializer.php');
+$data = (array) json_decode(file_get_contents('php://input'), true);
 
-$user = new Users( $db );
+$user = new Users($db);
 
-#  Check for rge requests method
-if ( $_SERVER[ 'REQUEST_METHOD' ] !== 'POST' ) {
-    header( 'HTTP/1.1 405 Method Not Allowed' );
-    header( 'Allow: POST' );
+
+$validKeys = ['mail'];
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    header('HTTP/1.1 405 Method Not Allowed');
+    header('Allow: POST');
     exit();
 }
 
-#  Check for params  if matches required parametes
-$validKeys = [ 'name', 'mail', 'phone', 'address', 'pword' ];
-$invalidKeys = array_diff( array_keys( $data ), $validKeys );
-if ( !empty( $invalidKeys ) ) {
-    foreach ( $invalidKeys as $key ) {
-        $errors[] = "$key is not a valid input field";
-    }
-
-    if ( !empty( $errors ) ) {
-
-        $user->respondUnprocessableEntity( $errors );
-        return;
-    }
-
+if (!$user->validateRequiredParams($data, $validKeys)) {
+    return;
 }
 
-#  Check for fields  if empty
-foreach ( $validKeys as $key ) {
-    if ( empty( $data[ $key ] ) ) {
-        $errors[] = ucfirst( $key ) . ' is required';
-    }
-    if ( !empty( $errors ) ) {
-
-        $user->respondUnprocessableEntity( $errors );
-        return;
-    } else {
-        $data[ $key ] = $user->sanitizeInput( $data[ $key ] );
-        # Sanitize input
-    }
-}
 #Your method should be here
 unset( $user );
 unset( $db );
-
